f9c156c81769093941292dc3f42a61556d41e798
AlexKiz <alexauvrus@gmail.com>
2021-09-06 14:15:36 +0300

    add interceptor on axios

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios'
+import { access } from 'fs';
 axios.defaults.baseURL = `${process.env.REACT_APP_API_URL}`
+
+axios.interceptors.request.use(
+  config => {
+    
+    const accessToken = localStorage.getItem('accessToken')
+
+    if(accessToken) { 
+
+      config.headers.Authorization = `Bearer ${accessToken}`
+
+    }
+
+    return config
+
+  }, error => {
+
+    Promise.reject(error);
+
+    }
+  );
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,7 +35,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
